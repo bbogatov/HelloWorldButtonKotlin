b8afd6698af6ad89e656d72ef546081e8bee6ce6
bbogatov <bogatov.bogdan@gmail.com>
2019-05-03 15:47:57 +0300

    change delete button to setVisibility.View.GONE

--- app/src/main/java/com/example/helloworldbuttonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/helloworldbuttonkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.helloworldbuttonkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
@@ -14,9 +15,7 @@ class MainActivity : AppCompatActivity() {
         val helloWorldButton: Button = findViewById(R.id.button)
 
         helloWorldButton.setOnClickListener {
-            val viewGroup: ViewGroup = helloWorldButton.parent as ViewGroup
-            viewGroup.removeView(helloWorldButton)
+            helloWorldButton.visibility = View.GONE
         }
     }
-
 }
